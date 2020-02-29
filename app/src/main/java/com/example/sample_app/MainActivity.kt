0e903be0b66f14386c6a4ff815dc626d0f4fe497
nmheine <rbw2950@gmail.com>
2020-03-01 00:30:46 +0900

    メモ帳リスト出力表示テスト実装

--- app/src/main/java/com/example/sample_app/MainActivity.kt
+++ app/src/main/java/com/example/sample_app/MainActivity.kt
@@ -1,12 +1,37 @@
 package com.example.sample_app
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.ListAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.sample_app.R.id
+import com.example.sample_app.R.layout.activity_main
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    val memoList: ArrayList<Memo> = arrayListOf<Memo>()
+    //var memoAdapter: MemoAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activity_main)
+        val memoListView: ListView = findViewById(R.id.ListView_Memo) as ListView
+        memoListView.adapter = MemoAdapter(this,memoList)
+
+    }
+
+    fun addMemo(v: View) {
+        val d= Date()
+        val m= Memo("件名Test", 1,"本文Test",d)
+        memoList.add(m)
+        Log.d("memolist_num",memoList.size.toString())
+        val memoListView: ListView = findViewById(R.id.ListView_Memo) as ListView
+        memoListView.adapter = MemoAdapter(this,memoList)
+//        val memoAdapter: MemoAdapter = MemoAdapter(this,memoList)
+//        memoListView.adapter=memoAdapter
+//        memoAdapter.notifyDataSetChanged()
     }
 }
